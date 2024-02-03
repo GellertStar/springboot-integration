b7fed5c59d20dd36a4abf924d4551d8fb348edcd
耿磊 <484503507@qq.com>
2024-02-03 10:54:09 +0800

    fix

--- mm-service/src/main/java/com/hehe/integration/user/service/impl/UserServiceImpl.java
+++ mm-service/src/main/java/com/hehe/integration/user/service/impl/UserServiceImpl.java
@@ -6,17 +6,17 @@ import com.hehe.integration.mapper.UserMapper;
 import com.hehe.integration.user.service.UserService;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
-    @Resource
-    private UserService userService;
+//    @Resource
+//    private UserService userService;
     @Override
     public List<User> getUsers() {
 //        List<User> users = getBaseMapper().getUsers();
-        List<User> users = userService.list();
+//        List<User> users = userService.list();
+        List<User> users = lambdaQuery().eq(User::getId, Integer.valueOf(1)).list();
         return users;
     }
 
--- mm-web/src/main/java/com/hehe/integration/MmWebApplication.java
+++ mm-web/src/main/java/com/hehe/integration/MmWebApplication.java
@@ -1,13 +1,23 @@
 package com.hehe.integration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  *  注：为了避免扫描路径不一致，请将启动类放在Root Package 即 com.hehe.integration
  */
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@ComponentScan(basePackages = "com.hehe.integration.*")
+@MapperScan("com.hehe.integration.mapper")
+@Slf4j
 public class MmWebApplication {
 
 	public static void main(String[] args) {
