b7fa6d84c275322cfca93c4012e5dcf73949cb1e
耿磊 <484503507@qq.com>
2024-02-22 18:34:56 +0800

    完善controller

--- mm-web/src/main/java/com/hehe/integration/user/UserController.java
+++ mm-web/src/main/java/com/hehe/integration/user/UserController.java
@@ -1,11 +1,18 @@
 package com.hehe.integration.user;
 
-import com.hehe.integration.common.dto.entity.R;
+import cn.hutool.core.collection.CollectionUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.hehe.integration.common.dto.response.Response;
+import com.hehe.integration.entity.User;
 import com.hehe.integration.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
@@ -20,11 +27,17 @@ public class UserController {
     UserService userService;
 
     @GetMapping("list")
-    public R list() {
+    public Response list() {
         try {
-            return R.isOk().data(userService.list());
+            List<User> listUser = userService.list();
+            List<User> voList = new ArrayList<>();
+            if(CollectionUtil.isNotEmpty(listUser)){
+                voList = JSON.parseObject(JSON.toJSONString(listUser),
+                        new TypeReference<List<User>>(){});
+            }
+            return Response.ok(voList);
         } catch (Exception e) {
-            return R.isFail(e);
+            return Response.fail();
         }
 
     }
